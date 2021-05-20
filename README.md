025fc944eb14ae3fdfb824660f71f968063d8e18
kupr744 <kilian.hammer@gmx.de>
2021-05-20 19:59:21 +0200

    working with python 3.9.0

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # WhatsUpElon
-get the latest tweets from elon on your command line
+
+get the 10 latest tweets from elon musk followed by a link to the tweet
+
+you could also get the tweets from someone else just change the following in main:
+
+```python
+    x = LookUp(44196397) #<-- replace twitter user id
+```
+
+## Requirements
+
+- you need a twitter developer account
+- a secret bearer token
+- put the bearer token in the secrets.txt 
+- now you are good to go
---
+++ WhatsUpElon.py
@@ -0,0 +1,37 @@
+import requests
+import time
+
+class LookUp:
+    def __init__(self, uid):
+        self.id = 0
+        self.url = "https://api.twitter.com/2/users/{}/tweets".format(uid)
+        with open("secrets.txt", "r") as file:
+            self.secret = file.read()
+
+    def get_tweets(self):
+        headers = {"Authorization": "Bearer {}".format(self.secret)}
+        data = requests.get(self.url, headers=headers)
+        if data.status_code != 200:
+            raise Exception("Request returned an error: {} {}".format(data.status_code, data.text))
+
+        return data.json()
+
+    def makepretty(self, data):
+        if (self.id != data['meta']['newest_id']):
+            for i in data['data']:
+                print("Elon: {}, https://twitter.com/elonmusk/status/{} \n".format(i['text'], i['id']))
+            print("tweets: {}".format(data['meta']['result_count']))        
+        
+        self.id = data['meta']['newest_id']
+
+
+def main():
+    x = LookUp(44196397)
+
+    while(True):
+        data = x.get_tweets()
+        x.makepretty(data)
+        time.sleep(60 * 5)
+
+if __name__ == "__main__":
+    main()
